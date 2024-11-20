e628e1dca1d4bb2f4e9c8621679ff653e27e7345
T.Aoyama <aoym@issp.u-tokyo.ac.jp>
2024-11-20 12:26:01 +0900

    update _main.py

--- src/SXRD/_main.py
+++ src/SXRD/_main.py
@@ -14,71 +14,25 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see http://www.gnu.org/licenses/.
 
-from sys import exit
-import importlib
-
+import sys
 import odatse
 
-from . import __version__
 from .sxrd import Solver
 
+
 def main():
-    import argparse
+    """
+    Main function to run the data-analysis software for quantum beam diffraction experiments
+    on 3D material structures. It parses command-line arguments, loads the input file,
+    selects the appropriate algorithm and solver, and executes the analysis.
+    """
 
-    parser = argparse.ArgumentParser(
-        description=(
-            "Data-analysis software of quantum beam "
-            "diffraction experiments for 2D material structure"
-        )
-    )
-    parser.add_argument("inputfile", help="input file with TOML format")
-    parser.add_argument("--version", action="version", version="odatse-SXRD {}, ODAT-SE {}".format(__version__, odatse.__version__))
+    info, run_mode = odatse.initialize()
 
-    mode_group = parser.add_mutually_exclusive_group()
-    mode_group.add_argument("--init", action="store_true", help="initial start (default)")
-    mode_group.add_argument("--resume", action="store_true", help="resume intterupted run")
-    mode_group.add_argument("--cont", action="store_true", help="continue from previous run")
-
-    parser.add_argument("--reset_rand", action="store_true", default=False, help="new random number series in resume or continue mode")
-
-    args = parser.parse_args()
-
-    file_name = args.inputfile
-    info = odatse.Info.from_file(file_name)
-
-    alg_table = {
-        "mapper": "mapper_mpi",
-        "minsearch": "min_search",
-    }
-
-    if "name" not in info.algorithm:
-        print("ERROR: algorithm.name not defined")
-        exit(1)
-    try:
-        aname = info.algorithm["name"]
-        algname = "odatse.algorithm.{}".format(alg_table.get(aname, aname))
-        algmod = importlib.import_module(algname)
-    except ModuleNotFoundError as e:
-        print("ERROR: {}".format(e, algname))
-        exit(1)
-
-    if args.init is True:
-        run_mode = "initial"
-    elif args.resume is True:
-        run_mode = "resume"
-        if args.reset_rand is True:
-            run_mode = "resume-resetrand"
-    elif args.cont is True:
-        run_mode = "continue"
-        if args.reset_rand is True:
-            run_mode = "continue-resetrand"
-    else:
-        run_mode = "initial"  # default
+    alg_module = odatse.algorithm.choose_algorithm(info.algorithm["name"])
 
     solver = Solver(info)
     runner = odatse.Runner(solver, info)
-    alg = algmod.Algorithm(info, runner, run_mode=run_mode)
-    result = alg.main()
+    alg = alg_module.Algorithm(info, runner, run_mode=run_mode)
 
-if __name__ == "__main__":
-    main()
+    result = alg.main()
